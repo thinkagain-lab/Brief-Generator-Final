96502c54f4c4ad39c8c5e37b67d97b858a06e910
SalmanTal <159783189+SalmanTal@users.noreply.github.com>
2024-08-21 17:06:35 +0530

    Some Unnecessary files have been deleted.

--- database_view.py
+++ database_view.py
@@ -1,12 +1,12 @@
-# from app import app, db  # Directly import app and db
-# from models import User  # Import your models
+from app import app, db  # Directly import app and db
+from models import User  # Import your models
 
-# # Create a context to run database queries
-# with app.app_context():
-#     users = User.query.all()
+# Create a context to run database queries
+with app.app_context():
+    users = User.query.all()
 
-#     for user in users:
-#         print(f"ID: {user.id}, Username: {user.username}, Name: {user.name}, Email: {user.email}, Bio: {user.bio}, Image File: {user.image_file}")
+    for user in users:
+        print(f"ID: {user.id}, Username: {user.username}, Name: {user.name}, Email: {user.email}, Bio: {user.bio}, Image File: {user.image_file}")
 
 
 # from app import app, db  # Directly import app and db
@@ -21,14 +21,14 @@
 
 
 
-from app import app, db  # Directly import app and db
-from models import BriefHistory  # Import your models
+# from app import app, db  # Directly import app and db
+# from models import BriefHistory  # Import your models
 
-# Create a context to run database queries
-with app.app_context():
-    users = BriefHistory.query.all()
+# # Create a context to run database queries
+# with app.app_context():
+#     users = BriefHistory.query.all()
 
-    for user in users:
-        print(f"ID: {user.id}, userid: {user.user_id}, type: {user.brief_type}, domain: {user.domain}, generated brief: {user.generated_brief}, time: {user.timestamp}")
+#     for user in users:
+#         print(f"ID: {user.id}, userid: {user.user_id}, type: {user.brief_type}, domain: {user.domain}, generated brief: {user.generated_brief}, time: {user.timestamp}")
 
 
