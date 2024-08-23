90a0215a0be9bffeac97906acf74c28c2f2808b5
SalmanTal <159783189+SalmanTal@users.noreply.github.com>
2024-08-23 19:01:49 +0530

    "commit"

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'DATABASE_URL'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///brief_generator.db'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///DATABASE_URL.db"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
